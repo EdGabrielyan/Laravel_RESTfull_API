7f6ab37b86d4733c94e9168e6aa2c076965ba0cd
EdGabrielyan <edgabrielyan01@gmail.com>
2024-08-19 18:04:52 +0400

    test 15

--- app/Http/Action/Product/ProductAction.php
+++ app/Http/Action/Product/ProductAction.php
@@ -4,10 +4,8 @@ namespace App\Http\Action\Product;
 
 use App\Http\Repository\Product\Read\ProductReadRepositoryInterface;
 use App\Http\Repository\Product\Write\ProductWriteRepositoryInterface;
-use App\Models\Product;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Gate;
 
 class ProductAction
 {
@@ -25,9 +23,6 @@ class ProductAction
 
     public function storeData(Collection $data): EloquentCollection
     {
-        Gate::define('store-product', function (Product $product) {
-            return auth()->id() === $product->user_id;
-        });
         return $this->productWriteRepository->Save($data);
     }
 
@@ -38,17 +33,11 @@ class ProductAction
 
     public function updateData(Collection $data, int $id): EloquentCollection
     {
-        Gate::define('update-product', function (Product $product) {
-            return auth()->id() === $product->user_id;
-        });
         return $this->productWriteRepository->update($data, $id);
     }
 
     public function deleteData(int $id)
     {
-        Gate::define('delete-product', function (Product $product) {
-            return auth()->id() === $product->user_id;
-        });
         return $this->productWriteRepository->delete($id);
     }
 }
